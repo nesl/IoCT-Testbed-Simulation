341ce71f4dffc325137b010671fe5bb2941ac770
brianw-0 <14112541+brianw-0@users.noreply.github.com>
2024-02-19 15:57:23 -0800

    Delete legacy directory

--- legacy/test.sh
+++
@@ -1,2 +0,0 @@
-source venv/bin/activate
-python external_send.py --intermediate_address $1 --intermediate_port $2 --destination_address $3
--- legacy/translator.sh
+++
@@ -1,4 +0,0 @@
-
-source venv/bin/activate
-# sudo python translator.py --device_id $1
-sudo python host_recv_test.py --veth_intf $1 --mininet_intf $2 --internal_ip $3
